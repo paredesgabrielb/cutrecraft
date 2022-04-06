5a5ee401e2af318f11b80b9d3824f4ec77efd845
GABRIEL PAREDES <gabriel_paredes@bhdleon.com.do>
2022-04-06 12:24:03 -0400

    Adding comments

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cutrecraft
+
+## before
+install python 3 and code editor in your system
+
+## Install
+
+pip install ursina
+
+## Run
+
+python cutre.py
--- cutre.py
+++ cutre.py
@@ -1,8 +1,10 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+# create a window
 app = Ursina()
 
+# class to simulate a cube
 class Voxel(Button):
     def __init__(self, position = (0,0,0)):
         super().__init__(
@@ -15,20 +17,24 @@ class Voxel(Button):
             highlight_color = color.lime,
         )
 
+    # interaction with mouse
     def input(self,key):
         if self.hovered:
+            # left click to create cube
             if key == "left mouse down":
                     voxel = Voxel(position=self.position + mouse.normal)
-
+            # right click to delete cube
             if key == "right mouse down":
                     destroy(self)
-    
-chunkSize = 16
 
+# create chunk or map   
+chunkSize = 16
 for z in range(chunkSize):
     for x in range(chunkSize):
         voxel = Voxel( position = (x,0,z))
 
+# create player in first person
 player = FirstPersonController()
 
+# start running the game
 app.run()
